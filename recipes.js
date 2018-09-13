b9eb150c89cb877a064a21368b18007f0f5de423
Amel <amelrodr@gmail.com>
2018-09-13 17:56:42 -0500

    PP MEX Eliot and Amel Juan Carlos

--- recipes.js
+++ recipes.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
+const express = require('express')
 const Schema   = mongoose.Schema;
 const data = require('./data.js')
+const Recipe = require('./models/Recipe')
+
+const app = express()
 
 mongoose.connect('mongodb://localhost/recipeApp')
   .then(() => {
@@ -8,4 +12,37 @@ mongoose.connect('mongodb://localhost/recipeApp')
   }).catch(err => {
     console.error('Error connecting to mongo', err)
   });
+  
+app.use('/new',(req,res)=>{
+  Recipe.create({
+    title: 'tamales',
+    level: 'UltraPro Chef',
+    ingredients: ['maiz','pollo','salsa'],
+    cousine: 'mexican',
+    dishType: 'breakfast',
+    duration: 4,
+    creator: 'aztecas'
+  })
+})
 
+app.get('/newMenu',(req,res)=>{
+  Recipe.insertMany(data)
+  .then(res=>{
+    console.log(recipes.title)
+  })
+  .catch()
+})
+
+app.get('/update/:id',(req,res)=>{
+  Recipe.findByIdAndUpdate(req.params.id,{duration:100})
+  console.log('Exito')
+})
+
+app.get('/:id/delete',(req,res)=>{
+  Recipe.findByIdAndRemove(re.params.id)
+  console.log('borrado')
+})
+
+app.listen(3100,()=>{
+  console.log('escuchando')
+})
